dc8cf45487c08f5304ec6b1aea664fe8d7fd24cd
Student <example@example.com>
2019-08-12 16:45:55 -0700

    still broken current user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Topic.destroy_all
+Post.destroy_all
+
+# 5.times do
+#   user_name = Faker::Artist.name
+#   user = User.new(:user_name => user_name)
+#   5.times do
+#     topic_title = Faker::Cosmere.aon
+#     new_topic = user.topics.new(:title => topic_title)
+#     new_topic.save
+#     5.times do
+#       post_body = Faker::GreekPhilosophers.quote
+#       new_post = new_topic.posts.new(:content_body => post_body)
+#       new_post.save
+#     end
+#   end
+# end
